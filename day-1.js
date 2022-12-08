d5cb5f173fb1e83c876f7c3239441d29f2125ee8
nitin verma <nitinvermamv61506@gmail.com>
2022-12-08 21:21:23 +0530

    2nd commit

---
+++ day-1.js
@@ -0,0 +1,25 @@
+//nn bb ss u
+// let a= null;
+// let b =123;
+// let c=Boolean;
+
+// let d= BigInt("567")
+// let e= "string"
+// let f= "symbol"
+// let u = undefined;
+// console.log(a,b,c,d,e,f,u)
+//these are premitives in js
+//object-non primitive data type
+// const item={
+//     "nitin": true,
+//     "rohit": false,
+//     "abc" :67,
+// }
+// console.log(item["nitin"])
+
+
+// ques1
+
+
+
+
---
+++ day2.js
@@ -0,0 +1,48 @@
+// operaters in js 
+// let x=5;
+// let y=6;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(x++);
+// console.log(x--);
+// console.log(++x);
+
+// assinment operators
+// let a = 5;
+// assinment=+a;
+// console.log(assinment);
+// let comp1=6;
+// let comp2="6";
+// console.log("comp1==comp2 is",comp1==comp2)
+// console.log("comp1!=comp2 is",comp1!=comp2)
+// console.log("comp1===comp2 is",comp1===comp2)
+// console.log("comp1!==comp2 is",comp1!==comp2)
+// logical operter 
+
+// let x=5;
+// let y=6;
+// console.log(x>y || x==y)
+
+// conditional expresion 
+let a=prompt("hey whats your age?")
+a= number.parsint(a)
+console.log(typeof a)
+if (a>0){
+    alert("this is valid age");
+}
+else if(a>9){
+    alert("you cant drive");
+
+}
+else{
+    alert("this is invalid age");
+}
+
+
+
+
+
+
